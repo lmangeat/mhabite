e3f7af27901e8bdfcabe6ea6be29d1f457fc7fb9
lmangeat <leon.mangeat@gmail.com>
2015-12-11 15:46:16 +0100

    Init Symfony

---
+++ README.md
@@ -0,0 +1,4 @@
+mhabite
+=======
+
+A Symfony project created on December 11, 2015, 3:36 pm.
---
+++ src/App/frontBundle/AppfrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\frontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppfrontBundle extends Bundle
+{
+}
